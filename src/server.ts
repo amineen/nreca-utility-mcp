9cce25d78b344a974c918f514cb3c7faa997d541
“Aaron <amineen64@hotmail.com>
2025-10-18 13:08:18 +0000

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import { env } from "process";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = env.PORT || 8085;
+
+if (env.NODE_ENV === "development") {
+  console.info(
+    "\x1b[32m%s\x1b[0m",
+    `✅ Server is running on http://localhost:${PORT}`
+  );
+}
+
+export default app;
